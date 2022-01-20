49cc1a746a0fbf3906a5585158ae5efea057d5f0
Timeuh <timotdu54@gmail.com>
2022-01-20 20:18:50 +0100

    deleted timer attempt

--- src/main/java/com/timeuh/dsuhc/DemonSlayerUHC.java
+++ src/main/java/com/timeuh/dsuhc/DemonSlayerUHC.java
@@ -2,10 +2,7 @@ package com.timeuh.dsuhc;
 
 import com.timeuh.dsuhc.commands.CommandTest;
 import com.timeuh.dsuhc.listeners.DSUHCListener;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitScheduler;
 
 public final class DemonSlayerUHC extends JavaPlugin {
 
@@ -17,19 +14,11 @@ public final class DemonSlayerUHC extends JavaPlugin {
         getCommand("gamebroadcast").setExecutor(new CommandTest());
         getCommand("spawn").setExecutor(new CommandTest());
         getServer().getPluginManager().registerEvents(new DSUHCListener(), this);
-
-        Bukkit.getScheduler().runTaskTimer(this, new BukkitRunnable() {
-            @Override
-            public void run() {
-                Bukkit.broadcastMessage("test");
-            }
-        }, 0,20*10);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
-
 
 }
