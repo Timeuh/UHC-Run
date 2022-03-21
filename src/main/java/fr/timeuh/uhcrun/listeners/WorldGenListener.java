2d2c718af8c0f5f521a629a65e2864b67f4f0e39
Timeuh <timotdu54@gmail.com>
2022-03-21 17:23:27 +0100

    added custom generation for iron ore, reworked BlockPopulator generation method

--- src/main/java/fr/timeuh/uhcrun/listeners/WorldGenListener.java
+++ src/main/java/fr/timeuh/uhcrun/listeners/WorldGenListener.java
@@ -23,66 +23,74 @@ public class WorldGenListener implements Listener {
     @EventHandler
     public void modifyOreGeneration(WorldInitEvent event){
         World world = event.getWorld();
-        BlockPopulator goldOreModifier = new BlockPopulator() {
+        BlockPopulator goldOreModifier = createCustomOrePopulator(60,9, Material.GOLD_ORE);
+        BlockPopulator ironOreModifier = createCustomOrePopulator(60,9, Material.IRON_ORE);
+        world.getPopulators().add(goldOreModifier);
+        world.getPopulators().add(ironOreModifier);
+    }
+
+    public BlockPopulator createCustomOrePopulator(int maxLayer, int minChance, Material oreMaterial) {
+        return new BlockPopulator() {
             @Override
             public void populate(World world, Random random, Chunk source) {
                 int chance, xLayer, zLayer, oreChance;
-                for (int yLayer = 1; yLayer < 60; yLayer+=2) {
-                    chance = random.nextInt(10)+1;
-                    if (chance >= 9) {
+                for (int yLayer = 1; yLayer < maxLayer; yLayer += 2) {
+                    chance = random.nextInt(10) + 1;
+                    if (chance >= minChance) {
                         xLayer = random.nextInt(16) + 1;
                         zLayer = random.nextInt(16) + 1;
 
-                        for (Material material : materialList){
+                        for (Material material : materialList) {
                             if (source.getBlock(xLayer, yLayer, zLayer).getType().equals(material)) {
                                 oreChance = random.nextInt(5);
-                                switch (oreChance){
+                                switch (oreChance) {
                                     case 4:
-                                        source.getBlock(xLayer, yLayer, zLayer).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer + 1, yLayer, zLayer).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer, yLayer, zLayer + 1).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer + 1, yLayer, zLayer + 1).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer, yLayer + 1, zLayer).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer + 1, yLayer + 1, zLayer).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer, yLayer + 1, zLayer + 1).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer + 1, yLayer + 1, zLayer + 1).setType(Material.GOLD_ORE, false);
+                                        source.getBlock(xLayer, yLayer, zLayer).setType(oreMaterial, false);
+                                        source.getBlock(xLayer + 1, yLayer, zLayer).setType(oreMaterial, false);
+                                        source.getBlock(xLayer, yLayer, zLayer + 1).setType(oreMaterial, false);
+                                        source.getBlock(xLayer + 1, yLayer, zLayer + 1).setType(oreMaterial, false);
+                                        source.getBlock(xLayer, yLayer + 1, zLayer).setType(oreMaterial, false);
+                                        source.getBlock(xLayer + 1, yLayer + 1, zLayer).setType(oreMaterial, false);
+                                        source.getBlock(xLayer, yLayer + 1, zLayer + 1).setType(oreMaterial, false);
+                                        source.getBlock(xLayer + 1, yLayer + 1, zLayer + 1).setType(oreMaterial, false);
                                         break;
 
                                     case 3:
-                                        source.getBlock(xLayer, yLayer, zLayer).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer + 1, yLayer, zLayer).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer, yLayer, zLayer + 1).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer + 1, yLayer, zLayer + 1).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer, yLayer + 1, zLayer).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer + 1, yLayer + 1, zLayer).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer, yLayer + 1, zLayer + 1).setType(Material.GOLD_ORE, false);
+                                        source.getBlock(xLayer, yLayer, zLayer).setType(oreMaterial, false);
+                                        source.getBlock(xLayer + 1, yLayer, zLayer).setType(oreMaterial, false);
+                                        source.getBlock(xLayer, yLayer, zLayer + 1).setType(oreMaterial, false);
+                                        source.getBlock(xLayer + 1, yLayer, zLayer + 1).setType(oreMaterial, false);
+                                        source.getBlock(xLayer, yLayer + 1, zLayer).setType(oreMaterial, false);
+                                        source.getBlock(xLayer + 1, yLayer + 1, zLayer).setType(oreMaterial, false);
+                                        source.getBlock(xLayer, yLayer + 1, zLayer + 1).setType(oreMaterial, false);
                                         break;
 
                                     case 2:
-                                        source.getBlock(xLayer, yLayer, zLayer).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer + 1, yLayer, zLayer).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer, yLayer, zLayer + 1).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer + 1, yLayer, zLayer + 1).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer, yLayer + 1, zLayer).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer + 1, yLayer + 1, zLayer).setType(Material.GOLD_ORE, false);
+                                        source.getBlock(xLayer, yLayer, zLayer).setType(oreMaterial, false);
+                                        source.getBlock(xLayer + 1, yLayer, zLayer).setType(oreMaterial, false);
+                                        source.getBlock(xLayer, yLayer, zLayer + 1).setType(oreMaterial, false);
+                                        source.getBlock(xLayer + 1, yLayer, zLayer + 1).setType(oreMaterial, false);
+                                        source.getBlock(xLayer, yLayer + 1, zLayer).setType(oreMaterial, false);
+                                        source.getBlock(xLayer + 1, yLayer + 1, zLayer).setType(oreMaterial, false);
                                         break;
 
                                     case 1:
-                                        source.getBlock(xLayer, yLayer, zLayer).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer + 1, yLayer, zLayer).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer, yLayer, zLayer + 1).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer + 1, yLayer, zLayer + 1).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer, yLayer + 1, zLayer).setType(Material.GOLD_ORE, false);
+                                        source.getBlock(xLayer, yLayer, zLayer).setType(oreMaterial, false);
+                                        source.getBlock(xLayer + 1, yLayer, zLayer).setType(oreMaterial, false);
+                                        source.getBlock(xLayer, yLayer, zLayer + 1).setType(oreMaterial, false);
+                                        source.getBlock(xLayer + 1, yLayer, zLayer + 1).setType(oreMaterial, false);
+                                        source.getBlock(xLayer, yLayer + 1, zLayer).setType(oreMaterial, false);
                                         break;
 
                                     case 0:
-                                        source.getBlock(xLayer, yLayer, zLayer).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer + 1, yLayer, zLayer).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer, yLayer, zLayer + 1).setType(Material.GOLD_ORE, false);
-                                        source.getBlock(xLayer + 1, yLayer, zLayer + 1).setType(Material.GOLD_ORE, false);
+                                        source.getBlock(xLayer, yLayer, zLayer).setType(oreMaterial, false);
+                                        source.getBlock(xLayer + 1, yLayer, zLayer).setType(oreMaterial, false);
+                                        source.getBlock(xLayer, yLayer, zLayer + 1).setType(oreMaterial, false);
+                                        source.getBlock(xLayer + 1, yLayer, zLayer + 1).setType(oreMaterial, false);
                                         break;
 
-                                    default: break;
+                                    default:
+                                        break;
                                 }
                             }
                         }
@@ -90,13 +98,11 @@ public class WorldGenListener implements Listener {
                 }
             }
         };
-        world.getPopulators().add(goldOreModifier);
     }
 
-    public static List<Material> createMaterialList() {
+    public List<Material> createMaterialList() {
         List<Material> list = new ArrayList<>();
         list.add(Material.STONE);
-        list.add(Material.GRAVEL);
         list.add(Material.SANDSTONE);
         return list;
     }
