2fb3c525faf4cede3a7e894c31a7b3877b9d33a6
Timeuh <timotdu54@gmail.com>
2022-03-28 22:54:00 +0200

    nether fortress good

--- src/main/java/fr/timeuh/uhcrun/worldgeneration/NetherChunk.java
+++ src/main/java/fr/timeuh/uhcrun/worldgeneration/NetherChunk.java
@@ -44,6 +44,27 @@ public class NetherChunk extends BlockPopulator {
             if ((x == 0 || x == 1 || x == 14 || x == 15) || (z == 0 || z == 1 || z == 14 || z == 15)){
                 return Material.NETHERRACK;
             }
+            if (y == 5 || y == 6 || y == 7 || y == 8 || y == 9 || y == 10 || y == 11 || y == 12 || y == 13 || y == 14 || y == 15 || y == 16 || y == 17 || y == 18){
+                if ((x == 6 || x == 7 || x == 8 || x == 9) && (z == 6 || z == 7 || z == 8 || z == 9)){
+                    return Material.NETHER_BRICK;
+                }
+            }
+            if (y == 19){
+                if ((x == 6 || x == 7 || x == 8 || x == 9) && (z >= 2 && z <= 15)){
+                    return Material.NETHER_BRICK;
+                }
+                if ((x >= 2 && x <= 15) && (z == 6 || z == 7 || z == 8 || z == 9)){
+                    return Material.NETHER_BRICK;
+                }
+            }
+            if (y == 20){
+                if ((x == 6 || x == 9) && ((z >= 2 && z <= 6) || (z >= 9 && z <= 15))){
+                    return Material.NETHER_FENCE;
+                }
+                if (((x >= 2 && x <= 6) || (x >= 9 && x <= 15)) && (z == 6 || z == 9)){
+                    return Material.NETHER_FENCE;
+                }
+            }
         }
         return Material.AIR;
     }
