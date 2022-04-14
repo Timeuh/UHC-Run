5ad416660ac0c788a8419245eec25ecda86b61bd
Timeuh <timotdu54@gmail.com>
2022-04-15 00:08:59 +0200

    Initialise V2

---
+++ src/main/java/fr/timeuh/Timeuh.java
@@ -0,0 +1,17 @@
+package fr.timeuh;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Timeuh extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
