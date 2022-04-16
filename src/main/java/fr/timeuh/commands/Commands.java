63df6d9fde1ba96944461177ff1464f3d55793a8
Timeuh <timotdu54@gmail.com>
2022-04-16 11:12:50 +0200

    ajustements sur les connexions et affichage pour la commande setState

--- src/main/java/fr/timeuh/commands/Commands.java
+++ src/main/java/fr/timeuh/commands/Commands.java
@@ -4,6 +4,8 @@ import fr.timeuh.UHCRun;
 import fr.timeuh.game.Start;
 import fr.timeuh.game.State;
 import fr.timeuh.game.Stop;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -36,18 +38,23 @@ public class Commands implements CommandExecutor {
                     switch (args[0]){
                         case "waiting":
                             uhcRun.setState(State.WAITING);
+                            Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "[UHCRun] " + ChatColor.GOLD + "State " + ChatColor.DARK_RED + "WAITING");
                             break;
                         case "starting":
                             uhcRun.setState(State.STARTING);
+                            Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "[UHCRun] " + ChatColor.GOLD + "State " + ChatColor.DARK_RED + "STARTING");
                             break;
                         case "playing":
                             uhcRun.setState(State.PLAYING);
+                            Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "[UHCRun] " + ChatColor.GOLD + "State " + ChatColor.DARK_RED + "PLAYING");
                             break;
                         case "pvp":
                             uhcRun.setState(State.PVP);
+                            Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "[UHCRun] " + ChatColor.GOLD + "State " + ChatColor.DARK_RED + "PVP");
                             break;
                         case "finish":
                             uhcRun.setState(State.FINISH);
+                            Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "[UHCRun] " + ChatColor.GOLD + "State " + ChatColor.DARK_RED + "FINISH");
                             break;
                     }
 
--- src/main/java/fr/timeuh/players/Connections.java
+++ src/main/java/fr/timeuh/players/Connections.java
@@ -10,6 +10,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Connections implements Listener {
     private UHCRun uhcRun;
@@ -23,6 +25,10 @@ public class Connections implements Listener {
         if (uhcRun.isState(State.WAITING) || uhcRun.isState(State.FINISH)){
             Player player = event.getPlayer();
             uhcRun.getPlayers().addPlayer(player.getUniqueId());
+            player.setGameMode(GameMode.ADVENTURE);
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, Integer.MAX_VALUE, 0, false, false));
+            player.setHealth(20);
+            event.setJoinMessage(null);
             Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "[UHCRun] " + ChatColor.GOLD + "Le joueur " + ChatColor.DARK_RED + player.getName() + ChatColor.GOLD + " rejoint les runners");
         }
     }
@@ -31,9 +37,11 @@ public class Connections implements Listener {
     public void onReconnection(PlayerJoinEvent event){
         if (uhcRun.isState(State.STARTING) || uhcRun.isState(State.PLAYING) || uhcRun.isState(State.PVP)){
             Player player = event.getPlayer();
+            event.setJoinMessage(null);
             if (uhcRun.getPlayers().containsLivePlayer(player.getUniqueId())){
                 Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "[UHCRun] " + ChatColor.GOLD + "Reconnexion du joueur " + ChatColor.DARK_RED + player.getName());
             } else {
+                for (PotionEffect effect : player.getActivePotionEffects()) player.removePotionEffect(effect.getType());
                 player.setGameMode(GameMode.SPECTATOR);
                 player.sendMessage(ChatColor.DARK_PURPLE + "[UHCRun] " + ChatColor.GOLD + "La partie est déjà démarrée");
                 uhcRun.getPlayers().addPlayer(player.getUniqueId());
@@ -47,6 +55,7 @@ public class Connections implements Listener {
             Player player = event.getPlayer();
             uhcRun.getPlayers().removePlayer(player.getUniqueId());
             if (uhcRun.getPlayers().containsLivePlayer(player.getUniqueId())) uhcRun.getPlayers().removeLivePlayer(player.getUniqueId());
+            event.setQuitMessage(null);
             Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "[UHCRun] " + ChatColor.GOLD + "Le joueur " + ChatColor.DARK_RED + player.getName() + ChatColor.GOLD + " quitte les runners");
         }
     }
