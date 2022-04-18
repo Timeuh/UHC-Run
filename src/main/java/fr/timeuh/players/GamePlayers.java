1fca06a29b25ac838e9c4bf32367be680532d9ab
Timeuh <timotdu54@gmail.com>
2022-04-18 18:56:59 +0200

    ajout des joueurs insensibles pour les tp

--- src/main/java/fr/timeuh/players/GamePlayers.java
+++ src/main/java/fr/timeuh/players/GamePlayers.java
@@ -7,10 +7,12 @@ import java.util.UUID;
 public class GamePlayers {
     private List<UUID> coPlayers;
     private List<UUID> alivePlayers;
+    private List<UUID> noDamagePlayers;
 
     public GamePlayers() {
         this.coPlayers = new ArrayList<>();
         this.alivePlayers = new ArrayList<>();
+        this.noDamagePlayers = new ArrayList<>();
     }
 
     public void addPlayer(UUID newPlayer){
@@ -21,12 +23,20 @@ public class GamePlayers {
         alivePlayers.add(newPlayer);
     }
 
+    public void addInvinciblePlayer(UUID newPlayer){
+        noDamagePlayers.add(newPlayer);
+    }
+
     public List<UUID> getCoPlayers() {
         return coPlayers;
     }
 
     public List<UUID> getAlivePlayers() {
         return alivePlayers;
+    }
+
+    public List<UUID> getInvinciblePlayers() {
+        return noDamagePlayers;
     }
 
     public void removePlayer(UUID delPlayer){
@@ -37,12 +47,20 @@ public class GamePlayers {
         alivePlayers.remove(delPlayer);
     }
 
+    public void removeInvinciblePlayer(UUID delPlayer){
+        noDamagePlayers.remove(delPlayer);
+    }
+
     public boolean containsPlayer(UUID presentPlayer){
         return coPlayers.contains(presentPlayer);
     }
 
     public boolean containsLivePlayer(UUID presentPlayer){
         return alivePlayers.contains(presentPlayer);
+    }
+
+    public boolean containsInvinciblePlayer(UUID presentPlayer){
+        return noDamagePlayers.contains(presentPlayer);
     }
 
 }
