1b94fcedb338d78f1944c1c4a52fcfeaa41cc176
Timeuh <timotdu54@gmail.com>
2022-04-21 18:57:19 +0200

    début d'ajout du scénario timber

---
+++ src/main/java/fr/timeuh/scenario/Timber.java
@@ -0,0 +1,71 @@
+package fr.timeuh.scenario;
+
+import fr.timeuh.UHCRun;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Timber implements Listener {
+
+    private UHCRun uhcRun;
+    private List<Material> logs;
+
+    public Timber(UHCRun uhcRun) {
+        this.uhcRun = uhcRun;
+        this.logs = getLogs();
+    }
+
+    @EventHandler
+    public void treeChop(BlockBreakEvent event){
+        if (uhcRun.getScenario().isEnabled(Scenario.TIMBER)){
+            Block block = event.getBlock();
+            for (Material log : logs){
+                if (block.getType().equals(log)){
+                    String treeType = getTreeType(block);
+                    if (treeType.equals("Big")){
+                        breakBigTree(block);
+                    } else if (treeType.equals("Thin")){
+                        breakThinTree(block);
+                    }
+                }
+            }
+        }
+    }
+
+    private List<Material> getLogs(){
+        List<Material> logs = new ArrayList<>();
+        logs.add(Material.LOG);
+        logs.add(Material.LOG_2);
+        return logs;
+    }
+
+    private String getTreeType(Block block){
+        if (block.getRelative(BlockFace.EAST).getType().equals(block.getType()) || block.getRelative(BlockFace.EAST).getType().equals(block.getType())
+                || block.getRelative(BlockFace.NORTH).getType().equals(block.getType()) || block.getRelative(BlockFace.SOUTH).getType().equals(block.getType())){
+            return "Big";
+        } else {
+            return "Thin";
+        }
+    }
+
+    private void breakThinTree(Block block){
+        Block foot = getTreeFoot(block);
+    }
+
+    private void breakBigTree(Block block){
+        Block foot = getTreeFoot(block);
+    }
+
+    private Block getTreeFoot(Block block){
+        while (!(block.getRelative(BlockFace.DOWN).getType().equals(Material.DIRT) || block.getRelative(BlockFace.DOWN).getType().equals(Material.GRASS))){
+            block = block.getRelative(BlockFace.DOWN);
+        }
+        return block;
+    }
+}
