4d4c208c2fd6450309a99c5a409752908e34d6a9
Timeuh <timotdu54@gmail.com>
2022-04-23 17:46:07 +0200

    timber ok pour les arbres simples

--- src/main/java/fr/timeuh/scenario/Timber.java
+++ src/main/java/fr/timeuh/scenario/Timber.java
@@ -1,12 +1,14 @@
 package fr.timeuh.scenario;
 
 import fr.timeuh.UHCRun;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,8 +31,10 @@ public class Timber implements Listener {
                 if (block.getType().equals(log)){
                     String treeType = getTreeType(block);
                     if (treeType.equals("Big")){
+                        event.setCancelled(true);
                         breakBigTree(block);
                     } else if (treeType.equals("Thin")){
+                        event.setCancelled(true);
                         breakThinTree(block);
                     }
                 }
@@ -56,6 +60,13 @@ public class Timber implements Listener {
 
     private void breakThinTree(Block block){
         Block foot = getTreeFoot(block);
+        Block up = foot;
+        while (!(up.getType().equals(Material.LEAVES_2) || up.getType().equals(Material.LEAVES))){
+            up = up.getRelative(BlockFace.UP);
+            foot.setType(Material.AIR);
+            Bukkit.getWorld("world").dropItemNaturally(foot.getLocation(), new ItemStack(Material.LOG));
+            foot = up;
+        }
     }
 
     private void breakBigTree(Block block){
