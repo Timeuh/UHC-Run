a0102be50725028df3397cc7dfc528761161c9d9
Timeuh <timotdu54@gmail.com>
2022-04-25 23:09:26 +0200

    distinction entre les gros et petits arbres, ainsi que si timber est activé ou non

--- src/main/java/fr/timeuh/changes/FastThings.java
+++ src/main/java/fr/timeuh/changes/FastThings.java
@@ -1,6 +1,7 @@
 package fr.timeuh.changes;
 
 import fr.timeuh.UHCRun;
+import fr.timeuh.scenario.Scenario;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -13,6 +14,8 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class FastThings implements Listener {
@@ -30,7 +33,16 @@ public class FastThings implements Listener {
             block.setType(Material.AIR);
             Bukkit.getWorld("world").dropItem(block.getLocation(), new ItemStack(Material.LOG));
 
-            if (checkSurroundings(block) && lastLog(block)) breakLeaves(block);
+            if (!(uhcRun.getScenario().isEnabled(Scenario.TIMBER))) {
+                if (checkSurroundings(block) && lastLog(block)) {
+                    String type = getTreeType(block);
+                    if (type.equals("Thin")) {
+                        breakLeaves(block);
+                    } else if (type.equals("Big")) {
+                        breakBigLeaves(block);
+                    }
+                }
+            }
         }
     }
 
@@ -98,5 +110,76 @@ public class FastThings implements Listener {
                 if (y > maxY) cancel();
             }
         }.runTaskTimer(uhcRun,0,1);
+    }
+
+    private void breakBigLeaves(Block block) {
+        World world = block.getWorld();
+        new BukkitRunnable() {
+            int maxX = block.getX()+3, maxY = block.getY()+3, maxZ = block.getZ()+3;
+            int x = block.getX()-3, y = block.getY()-3, z = block.getZ()-3;
+
+            @Override
+            public void run() {
+                Material blockType = world.getBlockAt(x, y, z).getType();
+                Block current = world.getBlockAt(x, y, z);
+                if (blockType.equals(Material.LEAVES) || blockType.equals(Material.LEAVES_2)){
+                    current.setType(Material.AIR);
+                    dropApples(current);
+                }
+
+                x ++;
+                if (x > maxX){
+                    x = block.getX()-3;
+                    z ++;
+                }
+                if (z > maxZ){
+                    x = block.getX()-3;
+                    z = block.getZ()-3;
+                    y ++;
+                }
+                if (y > maxY) cancel();
+            }
+        }.runTaskTimer(uhcRun,0,1);
+    }
+
+    private Block getTreeFoot(Block block){
+        while (!(block.getRelative(BlockFace.DOWN).getType().equals(Material.DIRT) || block.getRelative(BlockFace.DOWN).getType().equals(Material.GRASS))){
+            block = block.getRelative(BlockFace.DOWN);
+        }
+        return block;
+    }
+
+    private String getTreeType(Block block){
+        Block foot = getTreeFoot(block);
+        while  (!(foot.getRelative(BlockFace.UP).getType().equals(Material.LEAVES) || foot.getRelative(BlockFace.UP).getType().equals(Material.LEAVES_2))){
+            for (BlockFace face : getFaces()){
+                if (foot.getRelative(face).getType().equals(foot.getType())) return "Big";
+            }
+            foot = foot.getRelative(BlockFace.UP);
+        }
+        return "Thin";
+    }
+
+    private List<BlockFace> getFaces(){
+        List<BlockFace> allFaces = new ArrayList<>();
+        allFaces.add(BlockFace.EAST);
+        allFaces.add(BlockFace.EAST_NORTH_EAST);
+        allFaces.add(BlockFace.EAST_SOUTH_EAST);
+        allFaces.add(BlockFace.NORTH_EAST);
+        allFaces.add(BlockFace.NORTH_NORTH_EAST);
+        allFaces.add(BlockFace.SOUTH_EAST);
+        allFaces.add(BlockFace.SOUTH_SOUTH_EAST);
+
+        allFaces.add(BlockFace.WEST);
+        allFaces.add(BlockFace.WEST_NORTH_WEST);
+        allFaces.add(BlockFace.WEST_SOUTH_WEST);
+        allFaces.add(BlockFace.NORTH_NORTH_WEST);
+        allFaces.add(BlockFace.NORTH_WEST);
+        allFaces.add(BlockFace.SOUTH_SOUTH_WEST);
+        allFaces.add(BlockFace.SOUTH_WEST);
+
+        allFaces.add(BlockFace.NORTH);
+        allFaces.add(BlockFace.SOUTH);
+        return allFaces;
     }
 }
