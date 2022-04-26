67c2a99f7597c67e684d237d87ff85fe7d6bc683
Timeuh <timotdu54@gmail.com>
2022-04-26 19:56:09 +0200

    impossibilité d'enchant avec flamme/fire aspect et de faire des potions de force/lvl2

--- src/main/java/fr/timeuh/changes/ForbiddenThings.java
+++ src/main/java/fr/timeuh/changes/ForbiddenThings.java
@@ -1,9 +1,16 @@
 package fr.timeuh.changes;
 
 import fr.timeuh.UHCRun;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.LeavesDecayEvent;
+import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.inventory.BrewEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class ForbiddenThings implements Listener {
 
@@ -17,5 +24,24 @@ public class ForbiddenThings implements Listener {
     public void onDecay(LeavesDecayEvent event){
         event.getBlock().setData((byte) (event.getBlock().getData()%4));
         event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void noFire(EnchantItemEvent event){
+        event.getEnchantsToAdd().remove(Enchantment.FIRE_ASPECT);
+        event.getEnchantsToAdd().remove(Enchantment.ARROW_FIRE);
+    }
+
+    @EventHandler
+    public void potionRestriction(BrewEvent event){
+        for (ItemStack stack : event.getContents()){
+            if (stack.isSimilar(new ItemStack(Material.GLOWSTONE_DUST))){
+                Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "[UHCRun] " + ChatColor.GOLD + "Les potions niveau deux sont " + ChatColor.DARK_RED + "désactivées");
+                event.setCancelled(true);
+            } else if (stack.isSimilar(new ItemStack(Material.BLAZE_POWDER))){
+                Bukkit.broadcastMessage(ChatColor.DARK_PURPLE + "[UHCRun] " + ChatColor.GOLD + "Les potions de force sont " + ChatColor.DARK_RED + "désactivées");
+                event.setCancelled(true);
+            }
+        }
     }
 }
