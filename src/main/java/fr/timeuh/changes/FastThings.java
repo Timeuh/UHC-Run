49fc9676864f48c14c3bb0ff6997c951f4271cba
Timeuh <timotdu54@gmail.com>
2022-04-26 21:15:17 +0200

    changement de drop des mobs

--- src/main/java/fr/timeuh/changes/FastThings.java
+++ src/main/java/fr/timeuh/changes/FastThings.java
@@ -4,15 +4,19 @@ import fr.timeuh.UHCRun;
 import fr.timeuh.scenario.Scenario;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class FastThings implements Listener {
 
@@ -60,6 +64,39 @@ public class FastThings implements Listener {
         if (block.getType().equals(Material.STONE) || block.getType().equals(Material.SANDSTONE)){
             block.setType(Material.AIR);
             Bukkit.getWorld("world").dropItem(block.getLocation(), new ItemStack(Material.COBBLESTONE));
+        }
+    }
+
+    @EventHandler
+    public void changeMobDrops(EntityDeathEvent event){
+        Entity mob = event.getEntity();
+        World world = event.getEntity().getWorld();
+        ItemStack food = new ItemStack(Material.COOKED_BEEF, 2);
+        ItemStack leather = new ItemStack(Material.LEATHER);
+        ItemStack arrow = new ItemStack(Material.ARROW, 4);
+        ItemStack string = new ItemStack(Material.STRING, 2);
+        ItemStack feather = new ItemStack(Material.STRING, 2);
+        ItemStack bow = new ItemStack(Material.BOW, 1, (short) 200);
+        Random bowChance = new Random();
+
+        if (mob instanceof Cow || mob instanceof Pig || mob instanceof Zombie){
+            event.getDrops().removeAll(event.getDrops());
+            world.dropItem(mob.getLocation(), food);
+            world.dropItem(mob.getLocation(), leather);
+        } else if (mob instanceof Skeleton){
+            event.getDrops().removeAll(event.getDrops());
+            world.dropItem(mob.getLocation(), food);
+            if ((bowChance.nextInt(2)+1) == 2) world.dropItem(mob.getLocation(), bow);
+        } else if (mob instanceof Chicken){
+            event.getDrops().removeAll(event.getDrops());
+            world.dropItem(mob.getLocation(), food);
+            world.dropItem(mob.getLocation(), arrow);
+            world.dropItem(mob.getLocation(), feather);
+        } else if (mob instanceof Sheep){
+            event.getDrops().removeAll(event.getDrops());
+            world.dropItem(mob.getLocation(), food);
+            world.dropItem(mob.getLocation(), leather);
+            world.dropItem(mob.getLocation(), string);
         }
     }
 
