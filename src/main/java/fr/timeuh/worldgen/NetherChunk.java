173224fa7a27f0cb1f2b4a438649d51ab42ea6a0
Timeuh <timotdu54@gmail.com>
2022-04-29 23:53:11 +0200

    ajout et génération de mon propre schematic

--- src/main/java/fr/timeuh/worldgen/NetherChunk.java
+++ src/main/java/fr/timeuh/worldgen/NetherChunk.java
@@ -2,15 +2,11 @@ package fr.timeuh.worldgen;
 
 import com.sk89q.worldedit.*;
 import com.sk89q.worldedit.bukkit.BukkitWorld;
-import com.sk89q.worldedit.bukkit.WorldEditPlugin;
-import com.sk89q.worldedit.schematic.MCEditSchematicFormat;
-import com.sk89q.worldedit.world.DataException;
 import fr.timeuh.UHCRun;
-import org.bukkit.Bukkit;
-import org.bukkit.Chunk;
+import org.bukkit.*;
 import org.bukkit.Location;
-import org.bukkit.World;
 import org.bukkit.generator.BlockPopulator;
+
 
 import java.io.File;
 import java.io.IOException;
@@ -26,30 +22,27 @@ public class NetherChunk extends BlockPopulator {
     @Override
     public void populate(World world, Random random, Chunk source){
         if ((source.getX() % 10 == 0) && (source.getZ() % 10 == 0)) {
-            Location loc = source.getBlock(0,0,0).getLocation();
+            Location loc = source.getBlock(0,27,0).getLocation();
             paste(loc);
         }
     }
 
     private void paste(Location pasteLoc) {
-        File file = new File("D:\\Bureau\\All\\PluginsMinecraft\\UHCRunV2\\src\\main\\resources\\Nether.schematic");
+        File file = new File("D:\\Bureau\\All\\PluginsMinecraft\\UHCRunV2\\src\\main\\resources\\NetherChunk.schematic");
         EditSession session = new EditSession(new BukkitWorld(pasteLoc.getWorld()), 1000000);
         Vector loc = new Vector(pasteLoc.getX(), pasteLoc.getY(), pasteLoc.getZ());
-        Bukkit.getScheduler().runTaskLater(
-                uhcRun, new Runnable() {
-
-                    @Override
-                    public void run() {
-                        try {
-                            CuboidClipboard cc = CuboidClipboard.loadSchematic(file);
-                            cc.paste(session, loc, false);
-                        } catch (com.sk89q.worldedit.world.DataException | IOException | MaxChangedBlocksException e) {
-                            System.out.print("DataException while loading schematic!");
-                            e.printStackTrace();
-                        }
-                    }
-                }, 0
-            );
-
+        Bukkit.getScheduler().runTaskLater(uhcRun, new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    CuboidClipboard cc = CuboidClipboard.loadSchematic(file);
+                    cc.paste(session, loc, false);
+                } catch (com.sk89q.worldedit.world.DataException | IOException | MaxChangedBlocksException e) {
+                    System.out.print("DataException while loading schematic!");
+                    e.printStackTrace();
+                }
+            }
+            }, 0);
     }
+
 }
