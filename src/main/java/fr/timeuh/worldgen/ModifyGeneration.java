0ca93d922227da92a23853f7005fb53ded0f87b5
Timeuh <timotdu54@gmail.com>
2022-05-22 21:31:45 +0200

    ajout de NMS pour supprimer les biomes oceans

--- src/main/java/fr/timeuh/worldgen/ModifyGeneration.java
+++ src/main/java/fr/timeuh/worldgen/ModifyGeneration.java
@@ -1,16 +1,33 @@
 package fr.timeuh.worldgen;
 
 import fr.timeuh.UHCRun;
+import net.minecraft.server.v1_8_R3.BiomeBase;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.WorldInitEvent;
+
+import java.lang.reflect.Field;
 
 public class ModifyGeneration implements Listener {
     private UHCRun uhcRun;
 
     public ModifyGeneration(UHCRun uhcRun) {
         this.uhcRun = uhcRun;
+
+        try {
+            Field biomesField = BiomeBase.class.getDeclaredField("biomes");
+            biomesField.setAccessible(true);
+
+            if (biomesField.get(null) instanceof BiomeBase[]){
+                BiomeBase[] biomes = (BiomeBase[]) biomesField.get(null);
+                biomes[BiomeBase.DEEP_OCEAN.id] = BiomeBase.PLAINS;
+                biomes[BiomeBase.OCEAN.id] = BiomeBase.FOREST;
+                biomesField.set(null, biomes);
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     @EventHandler
